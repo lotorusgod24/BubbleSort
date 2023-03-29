2f8f94ec12677daa37f1c48486bb7644a3925508
lotorusgod24 <maximum.fm4@yandex.ru>
2023-03-29 21:04:06 +0300

    Bubble sort

---
+++ lib/bubble_sort.dart
@@ -0,0 +1,18 @@
+
+ List<int>bubbleSort(List<int> array){
+
+for(int i =0; i<array.length;++i){
+   for(int j = 0;j<array.length-1-i;++j){
+     if(array[j]>array[j+1]){
+       int k=array[j];
+       array[j] = array[j+1];
+       array[j+1]=k;  
+     }
+     else{continue;}
+   }
+ }
+  
+  return array;
+
+}
+  
