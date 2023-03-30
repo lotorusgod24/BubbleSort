76ea8d2c607e1622838e0415f728346f51ab70ab
lotorusgod24 <maximum.fm4@yandex.ru>
2023-03-31 00:54:19 +0300

    try to add user input method from console

--- bin/bubble_sort.dart
+++ bin/bubble_sort.dart
@@ -1,4 +1,3 @@
-
 import 'dart:math';
 
 import 'package:bubble_sort/Bubble_sort/bubble_sort.dart';
@@ -9,16 +8,23 @@ import 'package:bubble_sort/shaker_sort/shaker_sort.dart';
 void main() {
     final amount = 200;
     final Random rand = Random();
-    final length = 50;
+    final length = 10;
+
+
 
    List<int> bubbleArray = List.generate(length, (index) => rand.nextInt(amount), growable: false);
-   print("Before sort: ");
-   print(bubbleArray);
-   print("After sort: ");
-   //print(bubbleSort(bubbleArray));
-   //print(shakerSort(bubbleArray));
-   //print(selectSrot(bubbleArray));
-   print(insertSort(bubbleArray));
+
+  print("Before sort: ");
+  print(bubbleArray);
+  print("After sort: ");
+
+  //Dart is not support user input command from console
+  //uncomment sort what u want and start programm
+  
+     print(bubbleSort(bubbleArray));
+     //print(shakerSort(bubbleArray));
+     //print(selectSrot(bubbleArray));
+     //print(insertSort(bubbleArray));
 
 }
 
